f5e303f5792c43f2840ef33771cbc707cd38c5ea
Harley <hajeffries8288@student.pps.net>
2023-05-26 14:01:30 -0700

    WeirdLightGlich

--- Assets/Scripts/SC_PlayerController.cs
+++ Assets/Scripts/SC_PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class SC_PlayerController : MonoBehaviour
 {
@@ -11,72 +12,110 @@ public class SC_PlayerController : MonoBehaviour
     [HideInInspector] public GameObject task;
     [HideInInspector] public bool atackTask = false;
 
-    public GameObject thingyMcThing;
-    public GameObject AI;
-    public Texture2D textureThing;
-
     TextMeshProUGUI daysTxt;
     TextMeshProUGUI healthTxt;
     RaycastHit hit;
+    GameObject thingyMcThing;
+    GameObject aimThing;
+    GameObject AI;
     GameObject placeingObject;
     GameObject dayNight;
+    bool mainMenuMode = true;
 
     void Start()
     {
-        daysTxt = GameObject.Find("DaysTxt").GetComponent<TextMeshProUGUI>();
-        healthTxt = GameObject.Find("HealthTxt").GetComponent <TextMeshProUGUI>();
-        dayNight = GameObject.Find("DayNightCycleObject");
-        Cursor.lockState = CursorLockMode.Confined;
+        if (SceneManager.GetActiveScene().name != "MainMenu")
+        {
+            daysTxt = GameObject.Find("DaysTxt").GetComponent<TextMeshProUGUI>();
+            healthTxt = GameObject.Find("HealthTxt").GetComponent<TextMeshProUGUI>();
+            dayNight = GameObject.Find("DayNightCycleObject");
+            AI = GameObject.Find("P_HumanAI");
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        else Light.DontDestroyOnLoad(GameObject.Find("Directional Light"));
     }
 
     void Update()
     {
-        if (Input.GetButton("Fire2"))
+        if (!mainMenuMode)
         {
-            Cursor.SetCursor(textureThing, Vector2.zero, CursorMode.Auto);
-        }
-
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
-        {
-            if (Input.GetButtonDown("Fire1"))
+            //Test for gun implementation
+            if (Input.GetButton("Fire2"))
             {
-                if (!hit.transform.name.Contains("Zomble") && !placeingObject)
+                Cursor.visible = false;
+                aimThing.SetActive(true);
+                aimThing.transform.GetComponent<RectTransform>().position = Input.mousePosition;
+            }
+            else if (Input.GetButtonUp("Fire2"))
+            {
+                Cursor.visible = true;
+                aimThing.SetActive(false);
+            }
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
+                if (Input.GetButtonDown("Fire1"))
                 {
-                    setAIDestination = hit.point;
-                    task = hit.transform.gameObject;
-                    atackTask = false;
-                    thingyMcThing.transform.position = hit.point;
+                    if (!hit.transform.name.Contains("Zomble") && !placeingObject)
+                    {
+                        setAIDestination = hit.point;
+                        task = hit.transform.gameObject;
+                        atackTask = false;
+                        thingyMcThing.transform.GetComponent<MeshRenderer>().enabled = true;
+                        thingyMcThing.transform.position = hit.point;
+                    }
+                    else
+                    {
+                        task = hit.transform.gameObject;
+                        thingyMcThing.transform.GetComponent<MeshRenderer>().enabled = false;
+                        atackTask = true;
+                    }
                 }
-                else
+                if (Input.GetKeyDown(KeyCode.Q))
                 {
-                    task = hit.transform.gameObject;
-                    atackTask = true;
+                    GameObject AIHolding = AI.transform.GetComponent<SC_AI>().holding;
+                    if (AIHolding)
+                    {
+                        AIHolding.transform.parent = null;
+                        AIHolding.transform.GetComponent<BoxCollider>().enabled = true;
+                        AI.transform.GetComponent<SC_AI>().holding = null;
+                    }
                 }
             }
-            if (Input.GetKeyDown(KeyCode.Q))
+            else if (Input.GetButtonDown("Fire1"))
             {
-                GameObject AIHolding = AI.transform.GetComponent<SC_AI>().holding;
-                if (AIHolding)
-                {
-                    AIHolding.transform.parent = null;
-                    AIHolding.transform.GetComponent<BoxCollider>().enabled = true;
-                    AI.transform.GetComponent<SC_AI>().holding = null;
-                }
+                print("Player clicked nothing");
+                setAIDestination = new Vector3(999999999999, 0, 999999999999);
+                thingyMcThing.GetComponent<MeshRenderer>().enabled = false;
+                task = null;
             }
-        }
-        else if (Input.GetButtonDown("Fire1"))
-        {
-            print("Player clicked nothing");
-            setAIDestination = new Vector3(999999999999, 0, 999999999999);
-            task = null;
-        }
 
-        Interface();
+            Interface();
+        }
     }
 
     public void Interface()
     {
         daysTxt.text = "Days: " + dayNight.GetComponent<SC_DayNight>().days;
         healthTxt.text = "Health: " + AI.GetComponent<SC_AI>().health;
+    }
+
+    // Button methods
+    public void QuitGame()
+    {
+        if (Debug.isDebugBuild)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+
+    public void PlayGame()
+    {
+        mainMenuMode = false;
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
     }
 }
